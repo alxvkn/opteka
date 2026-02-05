3824f58e8a16b30919c2f9e3913d2563d7b9c47e
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-02-05 11:00:23 +0300

    new data

--- data/products.js
+++ data/products.js
@@ -14,19 +14,19 @@ export function getProducts() {
         {
             id: 0,
             name: 'Витамин C',
-            price: 376,
+            price: 256,
             imgSrc: 'assets/product_image.jpg',
         },
         {
             id: 1,
-            name: 'Средство для промывания и орошения полости носа для детей и взрослых Морская вода спрей Vitateka/Витатека 50мл',
-            price: 376,
+            name: 'Пластырь',
+            price: 30,
             imgSrc: 'assets/product_image.jpg',
         },
         {
             id: 2,
-            name: 'Средство для промывания и орошения полости носа для детей и взрослых Морская вода спрей Vitateka/Витатека 50мл',
-            price: 376,
+            name: 'Витамин B3',
+            price: 45,
             imgSrc: 'assets/product_image.jpg',
         },
         {
